04641f26c88b5261b89834b26be6cd91d9ead87b
SG-97 <1151103260@student.mmu.edu.my>
2019-06-09 12:43:44 +0800

    commit after reformating

---
+++ Chairmaster/codes-2/chairmasterjavascript.js
@@ -0,0 +1,8 @@
+function curtainDown (){
+  document.getElementById("overlay").style.display = "block";
+/*  document.getElementById("overlay").style.height = "100%";*/
+}
+
+function closebtn (){
+  document.getElementById("overlay").style.display = "none"
+}
