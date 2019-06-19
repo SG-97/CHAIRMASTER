0fd6d6263ed53fcd0712780bd4da05b494d1b03f
SG-97 <1151103260@student.mmu.edu.my>
2019-06-19 15:20:59 +0800

    chair-product page done+carousel

--- Chairmaster/codes-2/chairmasterjavascript.js
+++ Chairmaster/codes-2/chairmasterjavascript.js
@@ -1,8 +1,20 @@
 function curtainDown (){
   document.getElementById("overlay").style.display = "block";
+  
 /*  document.getElementById("overlay").style.height = "100%";*/
 }
 
 function closebtn (){
-  document.getElementById("overlay").style.display = "none"
+  document.getElementById("overlay").style.display = "none";
+
 }
+
+/************************************************************curtain link delay*****************************************************************/
+
+/*function delay (){
+  setTimeOut(newPage, 2000);
+}
+
+function newPage (){
+  document.location.assign("products.html");
+}*/
