8084474f13209bfd31dcce7ee54f502fd34f11e4
SG-97 <1151103260@student.mmu.edu.my>
2019-07-07 14:56:00 +0800

    created, modified(carousel +navbar) sivaraj.biz

--- Chairmaster/codes-2/chairmasterjavascript.js
+++ Chairmaster/codes-2/chairmasterjavascript.js
@@ -1,6 +1,6 @@
 function curtainDown (){
   document.getElementById("overlay").style.display = "block";
-  
+
 /*  document.getElementById("overlay").style.height = "100%";*/
 }
 
@@ -8,13 +8,3 @@ function closebtn (){
   document.getElementById("overlay").style.display = "none";
 
 }
-
-/************************************************************curtain link delay*****************************************************************/
-
-/*function delay (){
-  setTimeOut(newPage, 2000);
-}
-
-function newPage (){
-  document.location.assign("products.html");
-}*/
