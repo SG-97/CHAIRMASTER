8973c882343d4b3cdac59bab4558b459eb0ebc44
SG-97 <1151103260@student.mmu.edu.my>
2019-07-25 04:23:19 +0800

    Before Removing Carousel

--- sivarajbiz.tech/sivarajbiz.js
+++ sivarajbiz.tech/sivarajbiz.js
@@ -1,19 +1,41 @@
-window.onload= function () {
-  document.getElementById("prevBtn").style.visibility = "hidden";
-  document.getElementById("nextBtn").addEventListener("click",function (){
-  document.getElementById("prevBtn").style.visibility = "initial";
-  }
-);
+window.onload= function start () {
+  leftClickNoAppear();
 }
 
-document.getElementById("togBtn").addEventListener("click",function(){
+function leftClickNoAppear() {
+  document.getElementById("prevBtn").style.visibility = "hidden";
+  document.getElementById("nextBtn").addEventListener("click",leftClickAppear );
+}
+
+function leftClickAppear () {
+   document.getElementById("prevBtn").style.visibility = "initial";
+}
+
+
+document.getElementById("togBtn").addEventListener("click",dropDownCurtainBg)
+
+
+function dropDownCurtainBg(){
   var dropdown = document.getElementsByClassName("navbar-collapse"), i ;
    for (i=0;i<dropdown.length;i++){
       dropdown[i].style.backgroundColor = "rgba(0,0,0,0.9)";
       dropdown[i].style.opacity = "1";
       dropdown[i].zIndex = "10";
+    }
+}
 
- }
 
-  }
-);
+/*
+function getViewPort(){
+  var h,w,viewableH,viewableW;
+  h = window.innerHeight;
+  w = window.innerWidth;
+  viewableH = 0.8*h;
+  viewableW = 0.5*w;
+  document.getElementsByClassName("card-body")[0].style.width = viewableW;
+  document.getElementsByClassName("card-body")[0].style.height = viewableH;
+  console.log("width = " + viewableW);
+  console.log("Height = " + viewableH);
+
+}
+*/
