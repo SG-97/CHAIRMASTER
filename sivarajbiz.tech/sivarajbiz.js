268658ed9c436a58a8e095584b283fda04c827cf
SG-97 <1151103260@student.mmu.edu.my>
2019-08-02 20:36:03 +0800

    finished Learn More-pill tabs, jumbotron. Changed banner image(netflix home inspired). Verified google business listing

--- sivarajbiz.tech/sivarajbiz.js
+++ sivarajbiz.tech/sivarajbiz.js
@@ -1,19 +1,35 @@
-window.onload= function start () {
+/*window.onload= function start () {
   leftClickNoAppear();
 }
-
+*/
 document.getElementById("togBtn").addEventListener("click",dropDownCurtainBg)
 
 
 function dropDownCurtainBg(){
   var dropdown = document.getElementsByClassName("navbar-collapse"), i ;
    for (i=0;i<dropdown.length;i++){
-      dropdown[i].style.backgroundColor = "rgba(0,0,0,0.3)";
+      dropdown[i].style.backgroundColor = "rgba(0,0,0,0.8)";
       dropdown[i].style.paddingBottom = "2%";
       dropdown[i].zIndex = "10";
     }
 }
 
+
+/*
+  window.addEventListener("scrollY",fadeOut() {
+    getElementsByClassName("descriptionWrapper").style.opacity = "0";
+
+  }
+)
+/*window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+*/
 /*
 function leftClickNoAppear() {
   document.getElementById("prevBtn").style.visibility = "hidden";
